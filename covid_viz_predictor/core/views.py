218773a085e72dd4b0b526311854390ff4f178ce
sandipan898 <sandipan.das898@gmail.com>
2021-10-04 16:11:31 +0530

    updating

--- covid_viz_predictor/core/views.py
+++ covid_viz_predictor/core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse, render
+import requests
+import json
 # Create your views here.
 
 def dashboard_view(request):
-    return render(request, template_name='dashboard.html')
+    data = requests.get('https://corona.lmao.ninja/v2/all?yesterday')
+    world_wide = data.json()
+    # print(json.loads(world_wide))
+    return render(request, template_name='dashboard.html', context=world_wide)
