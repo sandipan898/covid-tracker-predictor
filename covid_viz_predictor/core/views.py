ee732d79be8dacfeab84d06fdf8a2c10d7cac5d9
sandipan898 <sandipan.das898@gmail.com>
2021-11-30 23:59:00 +0530

    Updating UI and fixing templates

--- covid_viz_predictor/core/views.py
+++ covid_viz_predictor/core/views.py
@@ -20,24 +20,27 @@ def dashboard_view(request):
         # historical_worldwide = requests.get("https://corona.lmao.ninja/v2/historical/all")
         historical_worldwide = requests.get("https://disease.sh/v3/covid-19/historical/all?lastdays=all").json()
         historical_worldwide_cases = historical_worldwide['cases']
-        historical_worldwide_dates = list(historical_worldwide_cases.keys())
-        historical_worldwide_cases = list(historical_worldwide_cases.values())
-        historical_worldwide_recovered = list(historical_worldwide['recovered'].values())
-        historical_worldwide_deaths = list(historical_worldwide['deaths'].values())
-        # print(historical_worldwide_dates.count(), historical_worldwide_cases.count())
+        historical_worldwide_dates = list(historical_worldwide_cases.keys())[::3]
+        historical_worldwide_cases = list(historical_worldwide_cases.values())[::3]
+        historical_worldwide_recovered = list(historical_worldwide['recovered'].values())[::3]
+        historical_worldwide_deaths = list(historical_worldwide['deaths'].values())[::3]
+        # print(historical_worldwide_dates[0])
     except Exception as e:
         print(e)
         historical_worldwide = {}
     
     try:
         countries= requests.get("https://disease.sh/v3/covid-19/countries").json()
+        # flag_list = [country['countryInfo']['flag'] for country in countries]
         print(countries[0])
     except Exception as e:
         print(e)
         countries = {}
-    
+        flag_list ={}
+
     context = {
         'countries_data': countries,
+        # 'flag_list': flag_list,
         'all_data_worldwide': all_data_worldwide,
         'historical_worldwide_dates': historical_worldwide_dates,
         'historical_worldwide_cases': historical_worldwide_cases,
